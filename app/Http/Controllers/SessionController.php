372b9a0d8dd793b1a41ad278f31384d99fce2e82
luca <luca.lorenzetti0@gmail.com>
2021-09-08 10:33:39 +0200

    Fix controller session

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -9,12 +9,8 @@ class SessionController extends Controller
     public function index(Request $request)
     {
       
-        if(!$request->session()->has('session')){
-            $request->session()->put("session", false);
-        }
-        else if($request->session()->get('session') == false){
-            $request->session()->put('session', true);
-        }
+        !$request->session()->has('session') ? $request->session()->put("session", false) : $request->session()->put('session', true);
+  
         return view('session');
     }
 }
